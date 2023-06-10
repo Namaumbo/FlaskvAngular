32e953360215c8ccfee4af7003901aeccd060f46
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2023-06-11 01:16:54 +0200

    agular setup

---
+++ frontend/todofrontend/src/app/completelist/completelist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-completelist',
+  templateUrl: './completelist.component.html',
+  styleUrls: ['./completelist.component.scss']
+})
+export class CompletelistComponent {
+
+}
